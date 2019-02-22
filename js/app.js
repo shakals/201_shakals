6afefe9d6de574b4a756b079b3d7fb6a4509bf91
Shan Kalyanasundaram <shakals@amazon.com>
2019-02-22 00:21:11 -0800

    Lab 3 project - More complex requirements built

--- js/app.js
+++ js/app.js
@@ -29,19 +29,26 @@ question[4] = 'Am I boring you (y/n)?';
 correctAnswer[4] = 'n';
 
 // Question 6 will go on a separate loop later. Check further down below.
-question[5] = 'I have a random number between 1-9 in mind. Take a guess!';
-correctAnswer[5] = Math.floor(Math.random * 10);
+var question6 = 'I have a random number between 1-9 in mind. Take a guess!';
+var correctAnswer6 = Math.floor(Math.random() * 10);
+
+// Question 7 will go on a separate loop later. Check further down below.
+var question7 = 'Now, let\'s check your General Knowledge. List 1 of the 7 continents.';
+var correctAnswer7 = ['asia', 'africa', 'europe', 'north america', 'south america', 'australia', 'antarctica'];
 
 var i=0;
+var userScore = 0;
+var firstFiveQuestionsScore = 0;
+var questionSixScore = 0;
 
 // Show the first 5 questions
 while (i<5) {
-
   userAnswer[i] = prompt(question[i]).toLowerCase();
   console.log(question[i], '; Correct Answer =', correctAnswer[i], '; User Answer =', userAnswer[i]);
 
   if (userAnswer[i] === correctAnswer[i]) {
     alert('Correct answer!');
+    userScore++;
   }
   else {
     alert('Wrong answer!');
@@ -49,65 +56,56 @@ while (i<5) {
 
   i++;
 }
+firstFiveQuestionsScore = userScore;
 
-/*
-// Question 1
-var question = 'Am I a developer (y/n)?';
-var answer = prompt(question).toLowerCase();
+// Show the 6th question - Random Number guessing game
+alert(question6);
+console.log(question6, '; Correct Answer =', correctAnswer6);
+for (i=1; i<=5; i++) {
+  userAnswer[i] = Number(prompt('Attempt ' + i));
+  console.log('User Answer =', userAnswer[i]);
 
-console.log(question, answer);
-if (answer === 'y' || answer === 'yes') {  
-  alert('Correct answer!');
-}
-else{
-  alert('Wrong answer!');
-}
-
-// Question 2
-question = 'Am I a COBOL developer (y/n)?';
-answer = prompt(question).toLowerCase();
-
-console.log(question, answer);
-if (answer === 'n' || answer === 'no') {  
-  alert('Correct answer!');
-}
-else{
-  alert('Wrong answer!');
-}
-
-// Question 3
-question = 'Am I a PYTHON developer (y/n)?';
-answer = prompt(question).toLowerCase();
-
-console.log(question, answer);
-if (answer === 'n' || answer === 'no') {  
-  alert('Correct answer!');
-}
-else{
-  alert('Wrong answer!');
+  if (userAnswer[i] === correctAnswer6) {
+    alert('Correct guess!');
+    questionSixScore++;
+    userScore++;
+    break;
+  }
+  else {
+    if (userAnswer[i] > correctAnswer6) {
+      alert('Too High! Try again. ' + (5-i) + ' attempts left.');
+    }
+    else {
+      alert('Too Low! Try again. ' + (5-i) + ' attempts left.');
+    }    
+  }  
 }
 
-// Question 4
-question = 'Am I a JAVA developer (y/n)?';
-answer = prompt(question).toLowerCase();
+var questionSevenScore = 0;
 
-console.log(question, answer);
-if (answer === 'n' || answer === 'no') {  
-  alert('Correct answer!');
-}
-else{
-  alert('Wrong answer!');
+// Show the 7th question - List a Continent game
+alert(question7);
+console.log(question7, '; Correct Answer =', correctAnswer7[0], ';', correctAnswer7[1], ';', correctAnswer7[2], ';', correctAnswer7[3], ';', correctAnswer7[4], ';', correctAnswer7[5], ';', correctAnswer7[6]);
+for (i=1; i<=6; i++) {
+  userAnswer[i] = prompt('Attempt ' + i).toLowerCase();
+  console.log('User Answer =', userAnswer[i]);
+
+  if (correctAnswer7.indexOf(userAnswer[i]) !== -1) {
+    alert('Correct!');
+    questionSevenScore++;
+    userScore++;
+    break;
+  }
+  else {    
+    alert('Nope! Try again. ' + (6-i) + ' attempts left.');
+  }
 }
 
-// Question 5
-question = 'Am I boring you (y/n)?';
-answer = prompt(question).toLowerCase();
 
-console.log(question, answer);
-if (answer === 'n' || answer === 'no') {  
-  alert('Correct answer! You are in the right place!');
-}
-else{
-  alert('Wrong answer! You don\'t have a choice. Ha ha ha!');
-}
-*/
+alert ('Your score: ' + userScore + ' out of 7');
+//alert'Correct Answer =', correctAnswer7[0], ';', correctAnswer7[1]), ';', correctAnswer7[2], ';', correctAnswer7[3], ';', correctAnswer7[4], ';', correctAnswer7[5], ';', correctAnswer7[6];
+
+document.getElementById('liFirstFiveQuestions').textContent = 'First Five Questions: ' + firstFiveQuestionsScore;
+document.getElementById('liRandomNumber').textContent = 'Random Number Game Score: ' + questionSixScore;
+document.getElementById('liContinents').textContent = 'Seven Continents Game Score: ' + questionSevenScore;
+document.getElementById('hTotalScore').textContent = 'Total Score = ' + userScore + ' out of 7 ' + userName;
